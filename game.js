00e264834a6ebd1e92499cac40b424116f4a742d
Xaox01 <studiocrystalgames06@gmail.com>
2023-06-15 04:04:08 +0200

    Add game scene

--- game.js
+++ game.js
@@ -1,89 +1,94 @@
 const gameConfig = {
     type: Phaser.AUTO,
     scale: {
-        mode: Phaser.Scale.FIT,
-        parent: 'game-container',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: window.innerWidth,
-        height: window.innerHeight
+      mode: Phaser.Scale.FIT,
+      parent: 'game-container',
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: window.innerWidth,
+      height: window.innerHeight
     },
     scene: {
-        preload: preload,
-        create: create
+      preload: preload,
+      create: create
     }
-};
-
-const game = new Phaser.Game(gameConfig);
-
-let settingsWindow;
-let backgroundMusic;
-let soundVolume = 0.1;
-let soundSlider;
-
-function preload() {
+  };
+  
+  const game = new Phaser.Game(gameConfig);
+  
+  let settingsWindow;
+  let backgroundMusic;
+  let soundVolume = 0.1;
+  let soundSlider;
+  
+  function preload() {
     this.load.image('background', './img/background.png');
     this.load.audio('button-sound', './sounds/settingsAudio.wav');
     this.load.audio('background-music', './music/theme.wav');
-}
-
-function create() {
+  }
+  
+  function create() {
     const background = this.add.image(0, 0, 'background').setOrigin(0);
     background.displayWidth = game.config.width;
     background.displayHeight = game.config.height;
-
+  
     const startButton = document.getElementById('start-button');
     const continueButton = document.getElementById('continue-button');
     const settingsButton = document.getElementById('settings-button');
-
+  
     [startButton, continueButton, settingsButton].forEach(button => {
-        button.addEventListener('mouseover', () => {
-            button.classList.add('highlight');
-        });
-
-        button.addEventListener('mouseout', () => {
-            button.classList.remove('highlight');
-        });
-
-        createButton(button, playButtonSound);
+      button.addEventListener('mouseover', () => {
+        button.classList.add('highlight');
+      });
+  
+      button.addEventListener('mouseout', () => {
+        button.classList.remove('highlight');
+      });
+  
+      createButton(button, playButtonSound);
     });
-
+  
     startButton.addEventListener('click', startGame);
     continueButton.addEventListener('click', continueGame);
     settingsButton.addEventListener('click', openSettings);
-
+  
     backgroundMusic = this.sound.add('background-music');
     backgroundMusic.play({ loop: true });
     backgroundMusic.setVolume(soundVolume);
-
+  
     soundSlider = document.getElementById('sound-slider');
     soundSlider.addEventListener('input', updateVolume);
-
+  
     const stopMusicButton = document.getElementById('stop-music-button');
     stopMusicButton.addEventListener('click', stopMusic);
-}
-
-function createButton(buttonElement, clickHandler) {
+  }
+  
+  function createButton(buttonElement, clickHandler) {
     buttonElement.addEventListener('click', () => {
-        playButtonSound();
-        clickHandler();
+      playButtonSound();
+      clickHandler();
     });
-}
-
-function startGame() {
+  }
+  
+  function startGame() {
     console.log('Rozpocznij grę');
-}
-
-function continueGame() {
+    if (!game.scene.isActive('gameScene')) {
+      game.scene.stop('menuScene');
+      game.scene.start('gameScene');
+      document.getElementById('menu-container').style.display = 'none';
+    }
+  }
+  
+  function continueGame() {
     console.log('Kontynuuj');
-}
-
-function openSettings() {
+  }
+  
+  function openSettings() {
     console.log('Otwórz ustawienia');
     const windowWidth = 400;
     const windowHeight = 300;
     const windowX = (game.config.width - windowWidth) / 2;
     const windowY = (game.config.height - windowHeight) / 2;
-
+  
     const settingsWindowElement = document.createElement('div');
     settingsWindowElement.style.width = windowWidth + 'px';
     settingsWindowElement.style.height = windowHeight + 'px';
@@ -97,7 +102,7 @@ function openSettings() {
     settingsWindowElement.style.fontFamily = "'Press Start 2P', cursive";
     settingsWindowElement.style.color = '#fff';
     settingsWindowElement.style.fontSize = '16px';
-
+  
     const closeButton = document.createElement('button');
     closeButton.textContent = 'Zamknij';
     closeButton.style.position = 'absolute';
@@ -113,14 +118,14 @@ function openSettings() {
     closeButton.style.fontFamily = "'Press Start 2P', cursive";
     closeButton.style.cursor = 'pointer';
     closeButton.classList.add('highlight');
-
+  
     createButton(closeButton, closeSettings);
-
+  
     const soundSliderLabel = document.createElement('label');
     soundSliderLabel.textContent = 'Głośność:';
     soundSliderLabel.style.display = 'block';
     soundSliderLabel.style.marginTop = '16px';
-
+  
     soundSlider = document.createElement('input');
     soundSlider.id = 'sound-slider';
     soundSlider.type = 'range';
@@ -129,31 +134,61 @@ function openSettings() {
     soundSlider.step = '0.1';
     soundSlider.value = soundVolume.toString();
     soundSlider.addEventListener('input', updateVolume);
-
+  
     soundSliderLabel.appendChild(soundSlider);
-
+  
     settingsWindowElement.appendChild(soundSliderLabel);
     settingsWindowElement.appendChild(closeButton);
     document.body.appendChild(settingsWindowElement);
-
+  
     settingsWindow = settingsWindowElement;
-}
-
-function updateVolume() {
+  }
+  
+  function updateVolume() {
     soundVolume = parseFloat(soundSlider.value);
     backgroundMusic.setVolume(soundVolume);
-}
-
-function closeSettings() {
+  }
+  
+  function closeSettings() {
     console.log('Zamknij ustawienia');
     settingsWindow.remove();
-}
-
-function playButtonSound() {
+  }
+  
+  function playButtonSound() {
     const buttonSound = new Audio('./sounds/settingsAudio.wav');
     buttonSound.play();
-}
-
-function stopMusic() {
+  }
+  
+  function stopMusic() {
     backgroundMusic.stop();
-}
+  }
+  
+  // Scena menu
+  class MenuScene extends Phaser.Scene {
+    constructor() {
+      super({ key: 'menuScene' });
+    }
+  }
+  
+  // Scena gry
+  class GameScene extends Phaser.Scene {
+    constructor() {
+      super({ key: 'gameScene' });
+    }
+  
+    create() {
+      // Inicjalizacja gry
+      const infoText = this.add.text(400, 300, 'Gra', { fontSize: '32px', fill: '#fff' });
+      infoText.setOrigin(0.5);
+    }
+  
+    update() {
+      // Logika gry
+    }
+  }
+  
+  // Dodanie scen do gry
+  game.scene.add('menuScene', MenuScene);
+  game.scene.add('gameScene', GameScene);
+  game.scene.start('menuScene');
+  
